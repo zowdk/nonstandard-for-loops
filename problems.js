25e2ace5901ceda4038824a421f9370ce7286afb
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-07-21 22:41:34 +0200

    solve + test problem set

--- problems.js
+++ problems.js
@@ -1,33 +1,57 @@
 function oddIndices(arr) {
   // Return an array containing all the odd indices in the array
-  // Your code here
+  let newArr = [];
+  for (let i = 1; i < arr.length; i += 2) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
 }
 
 function oddReverse(arr) {
   // Return an array containing all the odd indices starting from the end
-  // Your code here
+  let newArr = [];
+  for (let i = 1; i < arr.length; i += 2) {
+    newArr.push(arr[i]);
+  }
+  return newArr.reverse();
 }
 
 function secondPower(arr) {
   // Return an array containing all indices that are powers of 2
-  // Your code here
+  let squareArr = [];
+  for (let i = 1; i < arr.length; i *= 2) {
+    squareArr.push(arr[i]);
+  }
+  return squareArr;
 }
 
 function nthPower(arr, n) {
   // Return an array containing all indices that are powers of n
-  // Your code here
+  let nthPowerArr = [];
+  for (let i = 1; i < arr.length; i *= n) {
+    nthPowerArr.push(arr[i]);
+  }
+  return nthPowerArr;
 }
 
 function firstHalf(arr) {
   // Return an array containing the first half of an array
   // Include middle index on odd length arr
-  // Your code here
+  let newArr = [];
+  for (let i = 0; i < Math.floor((arr.length + 1) / 2); i += 1) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
 }
 
 function secondHalf(arr) {
   // Return an array containing the second half of an array
   // Exclude middle index on odd length arr
-  // Your code here
+  let halvedArr = [];
+  for (let i = Math.floor((arr.length + 1) / 2); i < arr.length; i++) {
+    halvedArr.push(arr[i]);
+  }
+  return halvedArr;
 }
 
 module.exports = {
